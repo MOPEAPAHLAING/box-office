24fe49506c85f3b0b71a8869e93b0fff12d1ab06
moepapahlaing <moepapahlaing21@gmail.com>
2020-12-28 12:00:55 +0630

    prepare show

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import Cast from '../components/show/Cast';
@@ -30,7 +31,7 @@ const Show = () => {
                 name={show.name}
                 rating={show.rating}
                 summary={show.summary}
-                tags={show.geners}
+                tags={show.genres}
             />
             <InfoBlock>
                 <h2>Details</h2>
@@ -42,11 +43,11 @@ const Show = () => {
             </InfoBlock>
             <InfoBlock>
                 <h2>Seasons</h2>
-                <Seasons seasons={show.embedded.seasons} />
+                <Seasons seasons={show._embedded.seasons} />
             </InfoBlock>
             <InfoBlock>
                 <h2>Casts</h2>
-                <Cast cast={show.embedded.cast} />
+                <Cast cast={show._embedded.cast} />
             </InfoBlock>
         </ShowPageWrapper>
     )
